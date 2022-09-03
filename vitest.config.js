9ae0c32c78321557e982500d32edebcf01869878
wenjielin <wenjielin@whale.im>
2022-09-03 22:08:24 +0800

    eslint

---
+++ vitest.config.js
@@ -0,0 +1,9 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import eslintPlugin from 'vite-plugin-eslint';
+
+
+// vite npm 包 这是一条注释
+export default defineConfig({
+  test: {}
+});
